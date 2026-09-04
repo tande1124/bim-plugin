83e17fc958a3b47e90a978ebaf59be4def1c812f
谭德 <tande@wuhanins.com>
2026-09-04 17:37:02 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 /**
  * 这里是框架入口钩子, 这里可以引入其它npm包，或某些全局操作。
  */
-export default {
-    install(app){
 
+import InsBimViewer from './components/InsBimViewer'
+export default {
+    install(app) {
+        app.component('InsBimViewer', InsBimViewer)
     }
 }
